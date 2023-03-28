047ae3d6eb72acb4bc750408341cb86c4b99432e
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2023-03-28 05:45:49 +0100

    feat<components>: Create a Base button component with necessary props

---
+++ src/components/BaseBtn.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ImgBuffer from './ImgBuffer.vue'
+defineProps(['loading'])
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+  <button :disabled="loading" class="btn-big" @click="emit('click')">
+    <slot v-if="!loading" />
+    <slot v-else name="loading">
+      <ImgBuffer class="h-6" />
+    </slot>
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
